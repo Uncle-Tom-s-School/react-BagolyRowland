9103c10b1e74e453dff403830a7b9cec3bff524b
Roland <you@example.com>
2025-10-21 09:22:45 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import BeerCard from "./components/BeerCard"
+import BeerCard, { type beolvasing } from "./components/BeerCard"
+import {use, useState, useEffect} from "react"
+
+const [beerData, setBeerData] = useState<beolvasing[]>([])
+
+
+useEffect(()=>{
+
+  fetch("data.json")
+  .then(response=>response.json())
+  .then((data)=>setBeerData(data))
+},[])
 
 const App = () => {
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {beerData.map(item => (<BeerCard{...item}/>))}
     </div>
   )
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,6 +1,22 @@
-const BeerCard = () => {
+export type beolvasing = {
+  image : string,
+  name : string,
+  abv : string,
+  volume: string,
+  price : number,
+  available : boolean
+}
+
+const BeerCard = (props: beolvasing) => {
   return (
-    <div className='beerCard'>Beer Card</div>
+    <div className='beerCard'>
+      <img src={props.image} alt="" />
+      <h2>{props.name}</h2>
+      <p>ABV: {props.abv}</p>
+      <p>Volume: {props.volume}</p>
+      <p>price: {props.price}</p>
+    
+    </div>
   )
 }
 
